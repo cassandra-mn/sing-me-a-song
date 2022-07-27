0852734eaf01c3dd3f7f384a334a567e17485203
cassandra-mn <arthenciac@gmail.com>
2022-07-27 15:31:48 -0300

    add integration tests

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -2,25 +2,90 @@ import supertest from 'supertest';
 
 import app from '../src/app.js';
 import {prisma} from '../src/database.js';
-import {createRecommendations} from './factories/generalFactory.js';
+import * as factory from './factories/generalFactory.js';
 
 const agent = supertest(app);
 
 beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY`;
 });
 
 describe('tests post recommendations', () => {
-    it('returns 422 for invalid parameters', async () => {
-        const body = {}; 
-        const response = await agent.post('/recommendations').send(body);
+    it('returns status 422 for invalid parameters', async () => {
+        const response = await agent.post('/recommendations').send({});
         expect(response.status).toEqual(422);
     });
     
-    it('returns 201 for valid parameters', async () => {
-        const body = createRecommendations();
+    it('returns status 409 for duplicate parameters', async () => {
+        const body = factory.createRecommendations();
+        await agent.post('/recommendations').send(body);
+        const response = await agent.post('/recommendations').send(body);
+        expect(response.status).toEqual(409);
+    });
+
+    it('returns status 201 for valid parameters', async () => {
+        const body = factory.createRecommendations();
         const response = await agent.post('/recommendations').send(body);
         expect(response.status).toEqual(201);
+    });
+
+    it('returns status 200 when upvote', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const id = factory.generateRandomNumber();
+        const response = await agent.post(`/recommendations/${id}/upvote`).send({});
+        expect(response.status).toEqual(200);
+    });
+
+    it('returns status 200 when downvote', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const id = factory.generateRandomNumber();
+        const response = await agent.post(`/recommendations/${id}/downvote`).send({});
+        expect(response.status).toEqual(200);
+    });
+
+    it('returns status 404 when score is less than -5', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const id = factory.generateRandomNumber();
+        await factory.upvoteOrDownvoteByCertainAmount(5, id, 'downvote');
+        const response = await agent.get(`/recommendations/${id}`);
+        expect(response.status).toEqual(404);
+    });
+});
+
+describe('tests get recommendations', () => {
+    it('returns the last 10 recommendations', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const response = await agent.get('/recommendations');
+        expect(response.body.length).toEqual(10);
+    });
+
+    it('returns recommendation by id', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const id = factory.generateRandomNumber();
+        const response = await agent.get(`/recommendations/${id}`);
+        expect(response.body.id).toEqual(id);
+    });
+
+    it('returns recommendations order by score', async () => {
+        const id = factory.generateRandomNumber();
+        const otherId = factory.generateRandomNumber();
+        await factory.createScenarioTwelveRecommendations();
+        await factory.upvoteOrDownvoteByCertainAmount(8, id, 'upvote');
+        await factory.upvoteOrDownvoteByCertainAmount(5, otherId, 'upvote');
+        const response = await agent.get('/recommendations/top/2');
+        expect(response.body[0].score).toBeGreaterThan(response.body[1].score);
+    });
+
+    it('returns random recommendations', async () => {
+        await factory.createScenarioTwelveRecommendations();
+        const response = await agent.get('/recommendations/random');
+        expect(response.body.id).toBeGreaterThanOrEqual(0);
+        expect(response.body.id).toBeLessThanOrEqual(12);
+    });
+
+    it('returns status 404 when no songs are registered', async () => {
+        const response = await agent.get('/recommendations/random');
+        expect(response.status).toEqual(404);
     });
 });
 
--- back-end/tests/factories/generalFactory.ts
+++ back-end/tests/factories/generalFactory.ts
@@ -1,8 +1,29 @@
 import {faker} from '@faker-js/faker';
+import supertest from 'supertest';
+
+import app from '../../src/app.js';
+
+const agent = supertest(app);
 
 export function createRecommendations() {
     return {
         name: faker.music.songName(),
         youtubeLink: `www.youtube.com/${faker.music.songName()}`
+    }
+}
+
+export async function createScenarioTwelveRecommendations() {
+    for (let i = 0; i < 12; i++) {
+        await agent.post('/recommendations').send(createRecommendations());
+    }
+}
+
+export function generateRandomNumber() {
+    return Math.floor(Math.random() * 13);
+}
+
+export async function upvoteOrDownvoteByCertainAmount(max: number, id: number, condition: string) {
+    for (let i = 0; i <= max; i++) {
+        await agent.post(`/recommendations/${id}/${condition}`).send({});
     }
 }
