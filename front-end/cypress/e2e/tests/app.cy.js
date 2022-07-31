fbb54771e2a5b714309859f0be62a15b3f693e3b
cassandra-mn <arthenciac@gmail.com>
2022-07-31 12:54:56 -0300

    refactore e2e tests

--- front-end/cypress/e2e/tests/app.cy.js
+++ front-end/cypress/e2e/tests/app.cy.js
@@ -4,34 +4,70 @@ beforeEach(() => {
     cy.resetDatabase();
 });
 
-describe('test the application', () => {
-    it('should display the recommendation after register', () => {
-        const recommendation = {
-            name: 'Good Vibes',
-            youtubeLink: 'https://youtu.be/J0DkaHJu4Js'
-        }
-        const otherRecommendation = {
-            name: 'Good Vibes 2.0',
-            youtubeLink: 'https://youtu.be/iAG1W7QJ9FA'
-        }
+const recommendation = {
+    name: 'Good Vibes',
+    youtubeLink: 'https://youtu.be/J0DkaHJu4Js'
+}
+const otherRecommendation = {
+    name: 'Good Vibes 2.0',
+    youtubeLink: 'https://youtu.be/iAG1W7QJ9FA'
+}
 
+describe('recommendation creation tests', () => {
+    it('creates a recommendation', () => {
         cy.createRecommendation(recommendation);
-        cy.wait(1000);
+
+        cy.contains(`${recommendation.name}`).should('be.visible');
+    });
+    
+    it('error creating recommendation', () => {
+        const recommendation = {name: 'ok', youtubeLink: 'ok'};
+        cy.createRecommendation(recommendation);
+        
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("Error creating recommendation!");
+        });
+    });
+});
+
+describe('tests upvote and downvote', () => {
+    it('should add upvotes', () => {
+        cy.createRecommendation(recommendation);
         cy.upvotes(15);
-        cy.wait(1000);
+        
+        cy.get('#score').should('contain.text', '15');
+    });
+
+    it('should add downvotes', () => {
+        cy.createRecommendation(recommendation);
         cy.downvotes(5);
-        
-        cy.wait(1000);
+
+        cy.get('#score').should('contain.text', '-5');
+    });
+    
+    it('should remove recommendation', () => {
+        cy.createRecommendation(recommendation);
+        cy.downvotes(6);
+
+        cy.get('#no-recommendations').contains('No recommendations yet! Create your own :)');
+    });
+});
+
+describe('tests application routes', () => {
+    it('get top recommendation', () => {
+        cy.createRecommendation(recommendation);
+        cy.upvotes(2);
+
         cy.createRecommendation(otherRecommendation);
-        cy.wait(1000);
-        cy.upvotes(8);
-        
-        cy.wait(1000);
-        cy.visit(URL + 'top');
 
-        cy.wait(1000);
-        cy.visit(URL + 'random');
+        cy.get('#top').click();
+        cy.get('#score').should('contain.text', '2');
+    });
 
-        //TODO: testes de quando as curtidas são menores que -5
+    it('get random recommendation', () => {
+        cy.createRecommendation(recommendation);
+
+        cy.get('#random').click();
+        cy.contains(`${recommendation.name}`).should('be.visible');
     });
 });
